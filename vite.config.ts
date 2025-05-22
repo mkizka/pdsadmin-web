500460448fd4eebd95b40f53cab426bf01557531
mkizka <g.mkizka@gmail.com>
2025-05-22 23:36:49 +0900

    previewのポート変更

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/pdsadmin-web/",
+  preview: {
+    port: 5173, // e2eテストのためにdevと同じポートにする
+  },
   plugins: [react(), tailwindcss()],
 });
