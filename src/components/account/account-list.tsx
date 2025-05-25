2d75bcd610bf015c9bf521d18fb74da2774f71bf
mkizka <g.mkizka@gmail.com>
2025-05-26 01:13:14 +0900

    Takedown表示追加

--- src/components/account/account-list.tsx
+++ src/components/account/account-list.tsx
@@ -14,7 +14,7 @@ import { InfiniteScroll } from "./infinite-scroll";
 
 function SkeltonListRow() {
   return (
-    <li className="list-row h-20 place-items-center gap-2">
+    <li className="list-row h-20 place-items-center gap-4">
       <div className="skeleton size-10 rounded-full"></div>
       <div className="flex w-full flex-col gap-2">
         <div className="skeleton h-4 w-full"></div>
@@ -41,7 +41,7 @@ function AccountListRow({ repo }: { repo: Repository | null }) {
 
   return (
     <li
-      className="list-row hover:bg-base-300 h-20 place-items-center gap-2 hover:cursor-pointer"
+      className="list-row hover:bg-base-300 h-20 place-items-center gap-4 hover:cursor-pointer"
       onClick={() => openModal({ type: "account-info", repo })}
       data-testid="account-list-row"
     >
@@ -51,9 +51,14 @@ function AccountListRow({ repo }: { repo: Repository | null }) {
         </div>
       </div>
       {/* palce-items-centerで全アイテムを縦横中央揃えした上で、2列目のみ横軸startに上書きする */}
-      <div className="justify-self-start">
-        <div className="font-bold" data-testid="account-list-row__handle">
-          @{repo.accountInfo.handle}
+      <div className="flex flex-col gap-1 justify-self-start">
+        <div className="flex items-center gap-2">
+          <div className="font-bold" data-testid="account-list-row__handle">
+            @{repo.accountInfo.handle}
+          </div>
+          {repo.repoInfo.status === "takendown" && (
+            <div className="badge badge-error badge-sm font-bold">Takedown</div>
+          )}
         </div>
         <div className="text-xs font-semibold opacity-60">
           at://{repo.repoInfo.did}
