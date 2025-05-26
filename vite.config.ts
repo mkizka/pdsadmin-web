07e01273ae46cedd2c4997ae9713a4176ab97433
mkizka <g.mkizka@gmail.com>
2025-05-26 12:38:04 +0900

    github pagesにもデプロイ

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE || "/",
   preview: {
     port: 5173, // e2eテストのためにdevと同じポートにする
   },
